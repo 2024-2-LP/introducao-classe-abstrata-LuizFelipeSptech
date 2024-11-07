a736ce2c6d0b48a2ef2553e631dc30a8d8bb717f
unknown <luiz.mendonca@sptech.school>
2024-11-07 17:55:13 -0300

    src/main/

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,40 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras = new ArrayList();
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double soma = 0.0;
+        for (Figura figura : figuras) {
+            soma += figura.calcularArea();
+        }
+        return soma;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasPequenas = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if(figura.calcularArea() > 20){
+                figurasPequenas.add(figura);
+            }
+        }
+        return figurasPequenas;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> quadrados = new ArrayList<>();
+        for (Figura figura : figuras) {
+            if (figura instanceof Quadrado ){
+                quadrados.add(figura);
+            }
+        }
+        return quadrados;
+    }
 }
